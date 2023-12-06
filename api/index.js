5c9d9725cf48d8194e480d8d978512212f1335a8
Leonard Akpofure <la.shivure@gmail.com>
2023-12-06 18:11:00 +0100

    Add user route and test endpoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -15,3 +16,12 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.get('/test', (req, res) => {
+    res.json({
+        message: 'Hello World',
+    });
+}
+);
+
+app.use('/api/user', userRouter);
