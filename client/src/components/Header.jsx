c8befd4c0e0d5c15b685c678050df7fd52eb49b7
Leonard Akpofure <la.shivure@gmail.com>
2023-12-06 12:15:03 +0100

    implement page routes in header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function H1() {
+  return (
+    <header className='bg-slate-300 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          <Link to='/'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>HA</span>
+                <span className='text-slate-800'>BRICKS LTD</span>
+            </h1>
+          </Link>
+        <form className='bg-slate-100 p-2 rounded-md flex items-center'>
+            <input type="text" placeholder="Search.." className='bg-transparent focus:outline-none w-24 sm:w-64' />
+            <FaSearch className='text-slate-500' />
+        </form>
+        <ul className='flex gap-4'>
+            <link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            </link>
+            <link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            </link>
+            <link to='/sign-out'>
+            <li className='sm:inline text-slate-700 hover:underline'>Sign In</li>
+            </link>
+        </ul>
+        </div>
+    </header>
+    
+  )
+}
