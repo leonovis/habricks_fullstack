807ff4bba98e92834e294caec96c1eddad3677a4
Leonard Akpofure <la.shivure@gmail.com>
2023-12-11 17:02:52 +0100

    sign In API route init

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,3 +14,22 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'user not found'));
+        // compareSync() returns true or false  if the password is valid or not
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(erro(401, 'wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httponly: true})
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
